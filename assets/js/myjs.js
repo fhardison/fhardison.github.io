785a9a0b3a429ae35308411bbb6eb7d5aaa53e4c
Fletcher <fhardison@gmail.com>
2019-06-27 08:41:56 -0400

    addign vue.js

---
+++ assets/js/myjs.js
@@ -0,0 +1,11 @@
+Vue.component('row', {
+	template: `<div class="row"><slot></slot></div>`});
+
+Vue.component('column', {
+	template: `<div class="column"><slot></slot></div>`});
+
+Vue.component('column-comment', {
+	template: `<div class="column-comment"><slot></slot></div>`});
+
+Vue.component('column-note', {
+	template: `<div class="column-note"><slot></slot></div>`});
